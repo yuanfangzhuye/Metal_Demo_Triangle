b8058e702a3a7f8bdf3ec25074343f913ad905ed
chao.li <chao.li@team-lab.cn>
2020-08-24 14:27:45 +0800

    Initial Commit

---
+++ Metal_Demo_Triangle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Metal_Demo_Triangle
+//
+//  Created by tlab on 2020/8/24.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Metal_Demo_Triangle/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Metal_Demo_Triangle
+//
+//  Created by tlab on 2020/8/24.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
